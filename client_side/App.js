9a7c8c7007e57e74d7becf505e81125dd63db286
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2023-02-09 18:09:55 -0500

    Created Splashscreen

--- client_side/App.js
+++ client_side/App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { useFonts, Inter_500Medium } from "@expo-google-fonts/inter";
+import * as Font from "expo-font";
+import { Inter_500Medium } from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Inter_500Medium,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({ Inter_500Medium });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch {
+        // handle error
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
   return (
     <SafeAreaView>
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={onLayout}>
         <Text style={styles.text}>NBA Player Stats Predictor</Text>
         <StatusBar style="auto" />
       </View>
