3af1309b3dbde4d77c3e9613950b78cbb033c82d
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2023-04-02 14:29:47 -0400

    Started search bar

--- client_side/App.js
+++ client_side/App.js
@@ -6,9 +6,12 @@ import * as SplashScreen from "expo-splash-screen";
 import { useCallback, useEffect, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import { LinearGradient } from "expo-linear-gradient";
+import { NBAPlayer } from "./components/Class";
+import axios from "axios";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
+  const [NBAPlayers, setNBAPlayers] = useState([]);
 
   useEffect(() => {
     async function prepare() {
@@ -24,6 +27,17 @@ export default function App() {
     }
     prepare();
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`http://192.168.2.15:5000/predict_season_stats/Lebron_James`)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   const onLayout = useCallback(() => {
     if (appIsReady) {
@@ -44,7 +58,10 @@ export default function App() {
         <View style={styles.container} onLayout={onLayout}>
           <StatusBar style="auto" />
           <Text style={styles.text}>NBA Player Stats Predictor</Text>
-          <SearchBar />
+          <SearchBar setNBAPlayers={setNBAPlayers} />
+          {NBAPlayers.map((player, index) => (
+            <Text key={index}>{player}</Text>
+          ))}
         </View>
       </SafeAreaView>
     </LinearGradient>
